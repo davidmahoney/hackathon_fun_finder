01bfd3a22d934223aaa7d2c5edbf759151b18bdf
David Mahoney <dmahoney@rdffg.bc.ca>
2013-10-19 16:03:20 -0700

    somewhat working map

--- hackathon/events/views.py
+++ hackathon/events/views.py
@@ -2,10 +2,17 @@
 from django.views.generic import TemplateView, DetailView, ListView
 import models
 
+
 class EventView(DetailView):
     template_name = "events/event_detail.html"
     model = models.Event
 
+
 class EventListView(ListView):
     template_name = "events/event_list.html"
+    model = models.Event
+
+
+class EventMap(ListView):
+    template_name = "events/map.html"
     model = models.Event
--- hackathon/urls.py
+++ hackathon/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.gis import admin
-from events.views import EventView, EventListView
+from events.views import EventView, EventListView, EventMap
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -10,8 +10,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hackathon.views.home', name='home'),
     # url(r'^hackathon/', include('hackathon.foo.urls')),
-    url(r'events/(?P<pk>\d+)/', EventView.as_view()),
-    url(r'events/$', EventListView.as_view()),
+    url(r'events/(?P<pk>\d+)/', EventView.as_view(), name="event_detail"),
+    url(r'events/$', EventListView.as_view(), name="event_list"),
+    url(r'events/map/$', EventMap.as_view(), name="event_map"),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
